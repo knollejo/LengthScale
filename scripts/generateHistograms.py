a6597a33ff7ef39ceedc735be763a6ca4682fa69
Joscha Knolle <joscha.knolle@cern.ch>
2016-12-19 14:23:51 +0100

    Bugfix

--- scripts/generateHistograms.py
+++ scripts/generateHistograms.py
@@ -51,7 +51,7 @@ def main():
     if args.combine and not args.gather:
         for action in args.actions:
             for scan in args.scans:
-                getattr(gather, action)(scan, combine=True)
+                getattr(gather+'PerBxStep', action)(scan, combine=True)
     if args.combine:
         args.combined = True
     if args.fit:
